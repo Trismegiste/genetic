93f42a921468881800eb1e62a28af6f7ca3facec
Trismegiste <flo@trismegiste.net>
2020-01-25 05:38:03 +0100

    dice roller ok

--- tests/Game/DiceRollerTest.php
+++ tests/Game/DiceRollerTest.php
@@ -9,4 +9,35 @@ class DiceRollerTest extends TestCase {
         $this->assertGreaterThan(0, DiceRoller::rollD10());
     }
 
+    public function testMultipleRoll() {
+        $s = 0;
+        for ($k = 0; $k < 1000; $k++) {
+            $s += DiceRoller::rollD10();
+        }
+        $s /= 1000;
+        $this->assertGreaterThan(5, $s);
+        $this->assertLessThan(7, $s);
+    }
+
+    /**
+     * @dataProvider getRolling
+     */
+    public function testMultipleRollAndKeep($r, $k, $avg) {
+        $s = 0;
+        for ($idx = 0; $idx < 1000; $idx++) {
+            $s += DiceRoller::rollAndKeep($r, $k);
+        }
+        $delta = ($s / 1000 - $avg) / $avg;
+        $this->assertLessThan(0.1, $delta);
+    }
+
+    public function getRolling() {
+        return[
+            [4, 2, 17],
+            [6, 3, 27],
+            [8, 4, 36],
+            [10, 5, 45]
+        ];
+    }
+
 }
