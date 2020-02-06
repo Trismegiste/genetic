ed215f31e6fc7042fb5d00123dde70a74ba7fb1d
Trismegiste <flo@trismegiste.net>
2020-02-06 04:45:15 +0100

    convergence OK

--- src/Command/L5rFree.php
+++ src/Command/L5rFree.php
@@ -16,7 +16,7 @@ class L5rFree extends L5rEvolve {
 
     // the name of the command
     protected static $defaultName = 'evolve:free';
-    protected $round = 3;
+    protected $round = 5;
 
     protected function configure() {
         $this->setDescription("Compute free evolution")
@@ -38,7 +38,7 @@ class L5rFree extends L5rEvolve {
                 'reflexe' => rand(2, 6),
                 'earth' => rand(2, 6),
                 'kenjutsu' => rand(1, 5),
-                'void' => rand(2, 4),
+                'void' => rand(2, 5),
                 'strength' => rand(2, 6)
             ]);
             $this->population[] = $pc;
@@ -61,12 +61,17 @@ class L5rFree extends L5rEvolve {
             }
 
             foreach ($this->population as $idx => $pc) {
-                if ($idx > (7 * $this->popSize / 10)) {
+                if ($idx <= ($this->popSize / 10)) {
+                    // we keep the best fit unchanged
+                    $pc->newGeneration();
+                } else if ($idx > (9 * $this->popSize / 10)) {
+                    // we clone & mutate the best fit to replace the worst fit
                     $npc = clone $this->population[rand(0, $this->popSize / 10)];
                     $npc->mutate();
+                    $npc->newGeneration();
                     $this->population[$idx] = $npc;
-                    $pc->newGeneration();
                 } else {
+                    // we mutate others
                     $pc->mutate();
                     $pc->newGeneration();
                 }
@@ -80,19 +85,25 @@ class L5rFree extends L5rEvolve {
                 if ($pc1 === $pc2) {
                     continue;
                 }
+                $delta = $pc1->getCost() - $pc2->getCost();
+
+                $key1 = spl_object_hash($pc1);
+                $key2 = spl_object_hash($pc2);
+                $win = [$key1 => 0, $key2 => 0];
                 for ($k = 0; $k < $this->round; $k++) {
                     $pc1->restart();
                     $pc2->restart();
                     $winner = $this->battle($pc1, $pc2);
-                    $delta = $pc1->getCost() - $pc2->getCost();
-
-                    if (($winner === $pc1) && ($delta < 0)) {
-                        $pc1->incVictory();
-                    }
-                    if (($winner === $pc2) && ($delta > 0)) {
-                        $pc2->incVictory();
-                    }
+                    $win[spl_object_hash($winner)] ++;
                 }
+
+                if (($win[$key1] > $win[$key2]) && ($delta <= 0)) {
+                    $pc1->incVictory();
+                }
+                if (($win[$key1] < $win[$key2]) && ($delta >= 0)) {
+                    $pc2->incVictory();
+                }
+                // many cases are missed : equality. We don't care, we want a threshold effect
             }
         }
     }
