47239efaad6b113af6a19b76c5abcbdb1e7aa76c
Trismegiste <flo@trismegiste.net>
2020-02-07 11:15:08 +0100

    more common code

--- src/Game/L5r/ComparedEcosystem.php
+++ src/Game/L5r/ComparedEcosystem.php
@@ -24,25 +24,7 @@ class ComparedEcosystem extends Ecosystem {
     protected function tournament($round) {
         foreach ($this->referencePop as $pc1) {
             foreach ($this->population as $pc2) {
-                $delta = $pc1->getCost() - $pc2->getCost();
-
-                $key1 = spl_object_hash($pc1);
-                $key2 = spl_object_hash($pc2);
-                $win = [$key1 => 0, $key2 => 0];
-                for ($k = 0; $k < $round; $k++) {
-                    $pc1->restart();
-                    $pc2->restart();
-                    $winner = $this->battle($pc1, $pc2);
-                    $win[spl_object_hash($winner)] ++;
-                }
-
-                if (($win[$key1] > $win[$key2]) && ($delta <= 0)) {
-                    $pc1->incVictory();
-                }
-                if (($win[$key1] < $win[$key2]) && ($delta >= 0)) {
-                    $pc2->incVictory();
-                }
-                // many cases are missed : equality. We don't care, we want a threshold effect
+                $this->evaluateBestFighter($round, $pc1, $pc2);
             }
         }
     }
--- src/Game/L5r/FreeEcosystem.php
+++ src/Game/L5r/FreeEcosystem.php
@@ -17,25 +17,7 @@ class FreeEcosystem extends Ecosystem {
                 if ($idx2 <= $idx1) {
                     continue;
                 }
-                $delta = $pc1->getCost() - $pc2->getCost();
-
-                $key1 = spl_object_hash($pc1);
-                $key2 = spl_object_hash($pc2);
-                $win = [$key1 => 0, $key2 => 0];
-                for ($k = 0; $k < $round; $k++) {
-                    $pc1->restart();
-                    $pc2->restart();
-                    $winner = $this->battle($pc1, $pc2);
-                    $win[spl_object_hash($winner)] ++;
-                }
-
-                if (($win[$key1] > $win[$key2]) && ($delta <= 0)) {
-                    $pc1->incVictory();
-                }
-                if (($win[$key1] < $win[$key2]) && ($delta >= 0)) {
-                    $pc2->incVictory();
-                }
-                // many cases are missed : equality. We don't care, we want a threshold effect
+                $this->evaluateBestFighter($round, $pc1, $pc2);
             }
         }
     }
