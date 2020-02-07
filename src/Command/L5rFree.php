c70c38e4634e8a3b656e5491a51aa1b89dc77141
Trismegiste <flo@trismegiste.net>
2020-02-07 08:06:15 +0100

    refactor business

--- src/Command/L5rFree.php
+++ src/Command/L5rFree.php
@@ -12,12 +12,14 @@ use Trismegiste\Genetic\Game\L5r\Property\VoidStrategy;
 /**
  * Free evolution
  */
-class L5rFree extends L5rEvolve {
+class L5rFree extends \Symfony\Component\Console\Command\Command {
 
     // the name of the command
     protected static $defaultName = 'evolve:free';
     protected $round;
     protected $extinctRatio;
+    protected $univers;
+    protected $maxGeneration;
 
     protected function configure() {
         $this->setDescription("Compute free evolution")
@@ -28,26 +30,12 @@ class L5rFree extends L5rEvolve {
     }
 
     public function initialize(InputInterface $input, OutputInterface $output) {
-        $this->popSize = $input->getArgument('popSize');
+        $popSize = $input->getArgument('popSize');
         $this->maxGeneration = $input->getArgument('maxIter');
         $this->round = $input->getOption('round');
         $this->extinctRatio = 1 - $input->getOption('extinct') / 100.0;
 
-        // init population for evolution
-        $this->population = [];
-        for ($k = 0; $k < $this->popSize; $k++) {
-            $pc = new Character('L5R', [
-                'voidStrat' => VoidStrategy::getRandomStrat(),
-                'stance' => Stance::getRandomStrat(),
-                'agility' => rand(2, 6),
-                'reflexe' => rand(2, 6),
-                'earth' => rand(2, 6),
-                'kenjutsu' => rand(1, 5),
-                'void' => rand(2, 5),
-                'strength' => rand(2, 6)
-            ]);
-            $this->population[] = $pc;
-        }
+        $this->univers = new \Trismegiste\Genetic\Game\L5r\Ecosystem($popSize);
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
@@ -55,59 +43,8 @@ class L5rFree extends L5rEvolve {
 
         for ($generation = 0; $generation < $this->maxGeneration; $generation++) {
             $output->writeln("======== Generation $generation ========");
-            $this->tournament();
-
-            usort($this->population, function($a, $b) {
-                return $b->getWinningCount() - $a->getWinningCount();
-            });
-
-            foreach ([0, 1, 2, 5, 9] as $idx) {
-                $output->writeln("$idx - " . $this->population[$idx]);
-            }
-
-            // re-initialise pop
-            foreach ($this->population as $pc) {
-                $pc->newGeneration();
-            }
-
-            // select & mutate
-            foreach ($this->population as $idx => $pc) {
-                if ($idx >= ($this->extinctRatio * $this->popSize)) {
-                    // we clone & mutate the best fit to replace the worst fit
-                    $npc = clone $this->population[$idx - $this->extinctRatio * $this->popSize];
-                    $npc->mutate();
-                    $this->population[$idx] = $npc;
-                }
-            }
-        }
-    }
-
-    protected function tournament() {
-        foreach ($this->population as $idx1 => $pc1) {
-            foreach ($this->population as $idx2 => $pc2) {
-                if ($idx2 <= $idx1) {
-                    continue;
-                }
-                $delta = $pc1->getCost() - $pc2->getCost();
-
-                $key1 = spl_object_hash($pc1);
-                $key2 = spl_object_hash($pc2);
-                $win = [$key1 => 0, $key2 => 0];
-                for ($k = 0; $k < $this->round; $k++) {
-                    $pc1->restart();
-                    $pc2->restart();
-                    $winner = $this->battle($pc1, $pc2);
-                    $win[spl_object_hash($winner)] ++;
-                }
-
-                if (($win[$key1] > $win[$key2]) && ($delta <= 0)) {
-                    $pc1->incVictory();
-                }
-                if (($win[$key1] < $win[$key2]) && ($delta >= 0)) {
-                    $pc2->incVictory();
-                }
-                // many cases are missed : equality. We don't care, we want a threshold effect
-            }
+            $report = $this->univers->evolve($this->round, $this->extinctRatio);
+            $output->writeln($report);
         }
     }
 
---
+++ src/Game/L5r/Ecosystem.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Trismegiste\Genetic\Game\L5r;
+
+/**
+ * Ecosystem of tournament for genetic algorithm
+ */
+class Ecosystem {
+
+    protected $population = [];
+
+    public function __construct($popSize) {
+        for ($k = 0; $k < $popSize; $k++) {
+            $pc = $this->createPc("L5R", [
+                'voidStrat' => Property\VoidStrategy::getRandomStrat(),
+                'stance' => Property\Stance::getRandomStrat(),
+                'agility' => rand(2, 6),
+                'reflexe' => rand(2, 6),
+                'earth' => rand(2, 6),
+                'kenjutsu' => rand(1, 5),
+                'void' => rand(2, 5),
+                'strength' => rand(2, 6)
+            ]);
+            $this->population[] = $pc;
+        }
+    }
+
+    public function createPc($name, $param = []) {
+        return new Character($name, $param);
+    }
+
+    public function getSize() {
+        return count($this->population);
+    }
+
+    public function evolve($round, $extinctRatio) {
+        // re-initialise pop
+        foreach ($this->population as $pc) {
+            $pc->newGeneration();
+        }
+
+        $this->tournament($round);
+
+        usort($this->population, function($a, $b) {
+            return $b->getWinningCount() - $a->getWinningCount();
+        });
+
+        $report = [];
+        foreach ([0, 1, 2, 5, 9] as $idx) {
+            $report[] = "$idx - " . $this->population[$idx];
+        }
+
+        // select & mutate
+        foreach ($this->population as $idx => $pc) {
+            if ($idx >= ($extinctRatio * $this->getSize())) {
+                // we clone & mutate the best fit to replace the worst fit
+                $npc = clone $this->population[$idx - $extinctRatio * $this->getSize()];
+                $npc->mutate();
+                $this->population[$idx] = $npc;
+            }
+        }
+
+        return $report;
+    }
+
+    protected function tournament($round) {
+        foreach ($this->population as $idx1 => $pc1) {
+            foreach ($this->population as $idx2 => $pc2) {
+                if ($idx2 <= $idx1) {
+                    continue;
+                }
+                $delta = $pc1->getCost() - $pc2->getCost();
+
+                $key1 = spl_object_hash($pc1);
+                $key2 = spl_object_hash($pc2);
+                $win = [$key1 => 0, $key2 => 0];
+                for ($k = 0; $k < $round; $k++) {
+                    $pc1->restart();
+                    $pc2->restart();
+                    $winner = $this->battle($pc1, $pc2);
+                    $win[spl_object_hash($winner)] ++;
+                }
+
+                if (($win[$key1] > $win[$key2]) && ($delta <= 0)) {
+                    $pc1->incVictory();
+                }
+                if (($win[$key1] < $win[$key2]) && ($delta >= 0)) {
+                    $pc2->incVictory();
+                }
+                // many cases are missed : equality. We don't care, we want a threshold effect
+            }
+        }
+    }
+
+    /**
+     * Battle between 2 PC
+     * 
+     * @param Character $pc1
+     * @param Character $pc2
+     * @return Character
+     */
+    protected function battle(Character $pc1, Character $pc2) {
+        $player = [];
+
+        $init1 = $pc1->rollInit();
+        $init2 = $pc2->rollInit();
+
+        if ($init1 === $init2) {
+            if (rand(1, 2) === 1) {
+                $init1++;
+            }
+        }
+
+        if ($init1 >= $init2) {
+            $player = [$pc1, $pc2];
+        } else {
+            $player = [$pc2, $pc1];
+        }
+
+        while (!$pc1->isDead() && !$pc2->isDead()) {
+            if (!$player[0]->isDead()) {
+                $player[1]->receiveAttack($player[0]);
+            }
+            if (!$player[1]->isDead()) {
+                $player[0]->receiveAttack($player[1]);
+            }
+        }
+
+        return $pc1->isDead() ? $pc2 : $pc1;
+    }
+
+}
