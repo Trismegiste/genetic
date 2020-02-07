3329db6c041bdd0ef5f0cdc9bc0f3187958f0d4c
Trismegiste <flo@trismegiste.net>
2020-02-07 12:09:41 +0100

    unit test for Free Evolve

---
+++ tests/Game/L5r/FreeEcosystemTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Trismegiste\Genetic\Game\L5r\FreeEcosystem;
+
+/**
+ * Test for FreeEcosystem
+ */
+class FreeEcosystemTest extends TestCase {
+
+    /** @dataProvider create */
+    public function testGetSize(FreeEcosystem $sut) {
+        $this->assertEquals(10, $sut->getSize());
+    }
+
+    /** @dataProvider create */
+    public function testEvolve(FreeEcosystem $sut) {
+        $report = $sut->evolve(3, 0.05);
+        $this->assertGreaterThanOrEqual(1, count($report));
+    }
+
+    public function create() {
+        return [[new FreeEcosystem(10)]];
+    }
+
+}
