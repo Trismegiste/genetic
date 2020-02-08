767c79dddeba1ab8ed3734edf1aa11513280b70b
Trismegiste <flo@trismegiste.net>
2020-02-08 03:56:54 +0100

    test grafx

--- src/Game/L5r/FreeEcosystem.php
+++ src/Game/L5r/FreeEcosystem.php
@@ -18,4 +18,41 @@ class FreeEcosystem extends Ecosystem {
         }
     }
 
+    protected function writeImage() {
+        $width = 1000;
+        $height = 1000;
+        $h = imagecreatetruecolor($width, $height);
+        $background = imagecolorallocate($h, 255, 255, 255);
+        $plotColor = imagecolorallocate($h, 0, 0, 0);
+        imagefill($h, 0, 0, $background);
+
+        $minWin = $minCost = 99999;
+        $maxWin = $maxCost = 0;
+        foreach ($this->population as $pc) {
+            $c = $pc->getCost();
+            $w = $pc->getWinningCount();
+            if ($c < $minCost) {
+                $minCost = $c;
+            }
+            if ($c > $maxCost) {
+                $maxCost = $c;
+            }
+            if ($w < $minWin) {
+                $minWin = $c;
+            }
+            if ($w > $maxWin) {
+                $maxWin = $c;
+            }
+        }
+
+        foreach ($this->population as $pc) {
+            /*     $x = 10 + ($pc->getCost() - $minCost) * ($width - 20) / ($maxCost - $minCost);
+              $y = 10 + ($pc->getWinningCount() - $minWin) * ($height - 20) / ($maxWin - $minWin); */
+            imagefilledellipse($h, 3 * $pc->getCost(), $height - $height * $pc->getWinningCount() / $this->getSize(), 4, 4, $plotColor);
+        }
+
+
+        imagepng($h, "population" . time() . ".png");
+    }
+
 }
