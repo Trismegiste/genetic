a6a0bf76ad189294aff9cd078196f219c9c11d21
Trismegiste <flo@trismegiste.net>
2020-02-08 05:05:23 +0100

    with plotting

--- src/Command/L5rFree.php
+++ src/Command/L5rFree.php
@@ -19,13 +19,15 @@ class L5rFree extends Command {
     protected $extinctRatio;
     protected $univers;
     protected $maxGeneration;
+    protected $plotFile;
 
     protected function configure() {
         $this->setDescription("Compute free evolution")
                 ->addArgument('popSize', InputArgument::REQUIRED, "Population size")
                 ->addArgument('maxIter', InputArgument::REQUIRED, "Max iteration")
                 ->addOption('round', NULL, InputArgument::OPTIONAL, 'How many round between 2 PC', 5)
-                ->addOption('extinct', NULL, InputArgument::OPTIONAL, 'Percentage of how many population are extinct between generation', 10);
+                ->addOption('extinct', NULL, InputArgument::OPTIONAL, 'Percentage of how many population are extinct between generation', 10)
+                ->addOption('plot', NULL, InputArgument::OPTIONAL, 'Percentage of how many population are extinct between generation', 'generation.png');
     }
 
     public function initialize(InputInterface $input, OutputInterface $output) {
@@ -33,6 +35,7 @@ class L5rFree extends Command {
         $this->maxGeneration = $input->getArgument('maxIter');
         $this->round = $input->getOption('round');
         $this->extinctRatio = $input->getOption('extinct') / 100.0;
+        $this->plotFile = $input->getOption('plot');
 
         $this->univers = new FreeEcosystem($popSize);
     }
@@ -47,43 +50,57 @@ class L5rFree extends Command {
             $output->writeln($report['text']);
             $grafx[$generation] = $report['grafx'];
         }
+
+        if (!is_null($this->plotFile)) {
+            $this->writeImage($grafx);
+        }
     }
 
-    protected function writeImage() {
-        $width = 1000;
-        $height = 1000;
+    protected function writeImage($data) {
+        $width = 1920;
+        $height = 1080;
         $h = imagecreatetruecolor($width, $height);
-        $background = imagecolorallocate($h, 255, 255, 255);
-        $plotColor = imagecolorallocate($h, 0, 0, 0);
+        $background = imagecolorallocate($h, 0, 0, 0);
         imagefill($h, 0, 0, $background);
 
         $minWin = $minCost = 99999;
         $maxWin = $maxCost = 0;
-        foreach ($this->population as $pc) {
-            $c = $pc->getCost();
-            $w = $pc->getWinningCount();
-            if ($c < $minCost) {
-                $minCost = $c;
-            }
-            if ($c > $maxCost) {
-                $maxCost = $c;
-            }
-            if ($w < $minWin) {
-                $minWin = $c;
-            }
-            if ($w > $maxWin) {
-                $maxWin = $c;
+        foreach ($data as $generation) {
+            foreach ($generation as $plot) {
+                $c = $plot['c'];
+                $w = $plot['w'];
+                if ($c < $minCost) {
+                    $minCost = $c;
+                }
+                if ($c > $maxCost) {
+                    $maxCost = $c;
+                }
+                if ($w < $minWin) {
+                    $minWin = $w;
+                }
+                if ($w > $maxWin) {
+                    $maxWin = $w;
+                }
             }
         }
 
-        foreach ($this->population as $pc) {
-            /*     $x = 10 + ($pc->getCost() - $minCost) * ($width - 20) / ($maxCost - $minCost);
-              $y = 10 + ($pc->getWinningCount() - $minWin) * ($height - 20) / ($maxWin - $minWin); */
-            imagefilledellipse($h, 3 * $pc->getCost(), $height - $height * $pc->getWinningCount() / $this->getSize(), 4, 4, $plotColor);
+        $deltaX = $minCost;
+        $deltaY = $minWin;
+        $scaleX = ($maxCost - $minCost) / ($width * 0.9);
+        $scaleY = ($maxWin - $minWin) / ($height * 0.9);
+
+        foreach ($data as $generation) {
+            $plotColor = imagecolorallocate($h, rand(127, 255), rand(127, 255), rand(127, 255));
+            foreach ($generation as $plot) {
+                $x = $width * 0.05 + ($plot['c'] - $deltaX) / $scaleX;
+                $y = $height - ($height * 0.05 + ($plot['w'] - $deltaY) / $scaleY);
+                imagefilledellipse($h, $x, $y, 4, 4, $plotColor);
+            }
+            imagecolordeallocate($h, $plotColor);
         }
 
-
-        imagepng($h, "population" . time() . ".png");
+        imagepng($h, "population.png");
+        imagedestroy($h);
     }
 
 }
