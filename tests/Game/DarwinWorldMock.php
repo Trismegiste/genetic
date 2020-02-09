12a53cba91d95fb9118b125f35a91f8363efcbe9
Trismegiste <flo@trismegiste.net>
2020-02-09 18:05:34 +0100

    subclass template method the right way

--- tests/Game/DarwinWorldMock.php
+++ tests/Game/DarwinWorldMock.php
@@ -8,14 +8,24 @@ use Trismegiste\Genetic\Game\DarwinWorld;
  */
 abstract class DarwinWorldMock extends DarwinWorld {
 
+    protected $build;
+
     public function __construct(TestCase $build) {
-        for ($k = 0; $k < 10; $k++) {
-            $pc = $build->getMockBuilder(Trismegiste\Genetic\Game\Mutable::class)->getMock();
-            $pc->expects($build->once())->method('newGeneration');
-            $pc->expects($build->once())->method('mutate');
-            $pc->expects($build->atLeastOnce())->method('getFitness');
-            $this->population[] = $pc;
+        $this->build = $build;
+        parent::__construct(10);
+    }
+
+    protected function createPopulation($popSize) {
+        $pop = [];
+        for ($k = 0; $k < $popSize; $k++) {
+            $pc = $this->build->getMockBuilder(Trismegiste\Genetic\Game\Mutable::class)->getMock();
+            $pc->expects($this->build->once())->method('newGeneration');
+            $pc->expects($this->build->once())->method('mutate');
+            $pc->expects($this->build->atLeastOnce())->method('getFitness');
+            $pop[] = $pc;
         }
+
+        return $pop;
     }
 
 }
