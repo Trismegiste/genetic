9c7d3987e104e72762df85d5a12e427f53a730ab
Trismegiste <flo@trismegiste.net>
2020-02-12 09:05:30 +0100

    clean

--- tests/Util/PlotterXYTest.php
+++ tests/Util/PlotterXYTest.php
@@ -1,20 +1,12 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use PHPUnit\Framework\TestCase;
+use Trismegiste\Genetic\Util\PlotterXY;
 
-/**
- * Description of PlotterXYTest
- *
- * @author flo
- */
-class PlotterXYTest extends \PHPUnit\Framework\TestCase {
+class PlotterXYTest extends TestCase {
 
     public function testWriteImage() {
-        $sut = new Trismegiste\Genetic\Util\PlotterXY(50, 50);
+        $sut = new PlotterXY(50, 50);
         $fch = tempnam(__DIR__, 'test');
         $this->assertFileExists($fch);
         $sut->writePng($fch);
