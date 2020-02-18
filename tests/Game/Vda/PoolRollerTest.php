eb89f7894aab8f7c4c6cefc78fc58755f2b9de23
Trismegiste <flo@trismegiste.net>
2020-02-18 08:58:44 +0100

    test for pool roller

--- tests/Game/Vda/PoolRollerTest.php
+++ tests/Game/Vda/PoolRollerTest.php
@@ -2,22 +2,33 @@
 
 namespace test\Vda;
 
-class PoolRollerTest extends \PHPUnit\Framework\TestCase {
+use PHPUnit\Framework\TestCase;
+use Trismegiste\Genetic\Game\Vda\PoolRoller;
+
+class PoolRollerTest extends TestCase {
 
     public function testZeroSuccess() {
-        $this->assertLessThanOrEqual(0, \Trismegiste\Genetic\Game\Vda\PoolRoller::roll(10, 11));
+        $this->assertLessThanOrEqual(0, PoolRoller::roll(10, 11));
     }
 
     public function testFullSuccess() {
-        $this->assertLessThanOrEqual(5, \Trismegiste\Genetic\Game\Vda\PoolRoller::roll(5, 1));
+        $this->assertLessThanOrEqual(5, PoolRoller::roll(5, 1));
     }
 
-    public function testAverage() {
+    public function testAverage1() {
         $sum = 0;
         for ($k = 0; $k < 100; $k++) {
-            $sum += \Trismegiste\Genetic\Game\Vda\PoolRoller::roll(1, 10);
+            $sum += PoolRoller::roll(1, 10);
         }
-        $this->assertLessThan(0.1, abs($sum / 1000));
+        $this->assertLessThan(0.1, abs($sum / 100));
+    }
+
+    public function testAverage2() {
+        $sum = 0;
+        for ($k = 0; $k < 100; $k++) {
+            $sum += PoolRoller::roll(1, 6);
+        }
+        $this->assertLessThan(0.1, abs($sum / 100) - 4);
     }
 
 }
