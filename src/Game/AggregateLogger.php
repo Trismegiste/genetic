a5c64badbcf4b8402a7eb9674cf795c85ec165e8
Trismegiste <flo@trismegiste.net>
2020-02-29 17:55:18 +0100

    aggregation of looger

---
+++ src/Game/AggregateLogger.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Trismegiste\Genetic\Game;
+
+/**
+ * Aggregate of multiple loggers
+ */
+class AggregateLogger implements PopulationLogger {
+
+    protected $logger;
+
+    public function __construct(array $log) {
+        foreach ($log as $idx => $item) {
+            if (!($item instanceof PopulationLogger)) {
+                throw new \InvalidArgumentException("Logger $idx is not a PopulationLogger");
+            }
+        }
+        $this->logger = $log;
+    }
+
+    public function endLog() {
+        foreach ($this->logger as $log) {
+            $log->endLog();
+        }
+    }
+
+    public function log(array &$pop) {
+        foreach ($this->logger as $log) {
+            $log->log($pop);
+        }
+    }
+
+}
