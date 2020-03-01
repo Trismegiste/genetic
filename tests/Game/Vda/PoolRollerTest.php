d10dc6ad4e3c1b017b31b31b0bb02d152b29b00c
Trismegiste <flo@trismegiste.net>
2020-03-01 21:35:03 +0100

    fix unlucky test

--- tests/Game/Vda/PoolRollerTest.php
+++ tests/Game/Vda/PoolRollerTest.php
@@ -7,6 +7,8 @@ use Trismegiste\Genetic\Game\Vda\PoolRoller;
 
 class PoolRollerTest extends TestCase {
 
+    const manyRoll = 666;
+
     public function testZeroSuccess() {
         $this->assertLessThanOrEqual(0, PoolRoller::roll(10, 11));
     }
@@ -17,18 +19,18 @@ class PoolRollerTest extends TestCase {
 
     public function testAverage1() {
         $sum = 0;
-        for ($k = 0; $k < 100; $k++) {
+        for ($k = 0; $k < self::manyRoll; $k++) {
             $sum += PoolRoller::roll(1, 10);
         }
-        $this->assertLessThan(0.1, abs($sum / 100));
+        $this->assertLessThan(0.1, abs($sum / self::manyRoll));
     }
 
     public function testAverage2() {
         $sum = 0;
-        for ($k = 0; $k < 100; $k++) {
+        for ($k = 0; $k < self::manyRoll; $k++) {
             $sum += PoolRoller::roll(1, 6);
         }
-        $this->assertLessThan(0.1, abs($sum / 100) - 4);
+        $this->assertLessThan(0.1, abs($sum / self::manyRoll) - 4);
     }
 
 }
