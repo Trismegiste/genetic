49481ab502e9a21fec30d3054549ee03c5d02631
Trismegiste <flo@trismegiste.net>
2020-03-01 18:42:10 +0100

    refactor sawo cmd

--- src/Command/GameFree.php
+++ src/Command/GameFree.php
@@ -3,6 +3,7 @@
 namespace Trismegiste\Genetic\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
--- src/Command/SaWoFree.php
+++ src/Command/SaWoFree.php
@@ -18,55 +18,31 @@ use Trismegiste\Genetic\Util\PlotterXY;
 /**
  * Free evolution for SaWo
  */
-class SaWoFree extends Command {
+class SaWoFree extends GameFree {
 
     protected static $defaultName = 'sawo:free';
-    protected $round;
-    protected $extinctRatio;
-    protected $univers;
-    protected $maxGeneration;
-    protected $plotFile;
-    protected $logger;
 
     protected function configure() {
-        $this->setDescription("Compute free evolution for SaWo")
-                ->addArgument('popSize', InputArgument::REQUIRED, "Population size")
-                ->addArgument('maxIter', InputArgument::REQUIRED, "Max iteration")
-                ->addOption('round', NULL, InputOption::VALUE_REQUIRED, 'How many round between 2 PC', 5)
-                ->addOption('extinct', NULL, InputOption::VALUE_REQUIRED, 'Percentage of how many population are extinct between generation', 5)
-                ->addOption('plot', NULL, InputOption::VALUE_REQUIRED, 'File name of plotting PNG picture')
-                ->addOption('animate', NULL, InputOption::VALUE_NONE, 'Multiple PNG file for animation');
-    }
-
-    public function initialize(InputInterface $input, OutputInterface $output) {
-        $popSize = $input->getArgument('popSize');
-        $this->maxGeneration = $input->getArgument('maxIter');
-        $this->round = $input->getOption('round');
-        $this->extinctRatio = $input->getOption('extinct') / 100.0;
-        $plotFile = $input->getOption('plot');
-
-        $this->logger = new AggregateLogger([new TextLogger($output, $this->extinctRatio)]);
-        if (!is_null($plotFile)) {
-            if ($input->getOption('animate')) {
-                $plotter = new AnimateXY(1920, 1080, $plotFile . '%04d.png');
-            } else {
-                $plotter = new PlotterXY(1920, 1080, $plotFile . '.png');
-            }
-            $this->logger->push(new GrafxLogger($plotter));
-        }
-
-        $this->univers = new FreeEcosystem($popSize, new CharacterFactory(), $this->logger);
+        parent::configure();
+        $this->setDescription("Compute free evolution for SaWo");
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
+        $popSize = $input->getArgument('popSize');
+        $maxGeneration = $input->getArgument('maxIter');
+        $round = $input->getOption('round');
+        $extinctRatio = $input->getOption('extinct') / 100.0;
+        $logger = $this->buildLogger($input, $output);
+        $univers = new FreeEcosystem($popSize, new CharacterFactory(), $logger);
+
         $output->writeln("Free evolution");
 
-        for ($generation = 0; $generation < $this->maxGeneration; $generation++) {
+        for ($generation = 0; $generation < $maxGeneration; $generation++) {
             $output->writeln("======== Generation $generation ========");
-            $this->univers->evolve($this->round, $this->extinctRatio);
+            $univers->evolve($round, $extinctRatio);
         }
 
-        $this->logger->endLog();
+        $logger->endLog();
     }
 
 }
