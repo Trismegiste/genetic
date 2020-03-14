4e1aa3cf2ee64e87cfe7e4d256de67ab179d12f9
Trismegiste <flo@trismegiste.net>
2020-03-14 15:44:22 +0100

    gamertest ok

--- tests/Command/GameFreeTest.php
+++ tests/Command/GameFreeTest.php
@@ -5,6 +5,8 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use Trismegiste\Genetic\Command\GameFree;
 use Trismegiste\Genetic\Game\DarwinWorld;
+use Trismegiste\Genetic\Game\Fighter;
+use Trismegiste\Genetic\Game\MutableFighter;
 use Trismegiste\Genetic\Game\MutableFighterFactory;
 use Trismegiste\Genetic\Game\PopulationLogger;
 
@@ -21,7 +23,11 @@ class GameFreeTest extends TestCase {
                     'popSize' => 10,
                     'maxIter' => 5,
                     '--round' => 3,
-                    '--extinct' => 0.5
+                    '--extinct' => 0.5,
+                    '--plot' => true,
+                    '--animate' => true,
+                    '--stat' => true,
+                    '--dump' => true
         ]));
     }
 
@@ -37,13 +43,54 @@ class MockCommand extends GameFree {
     }
 
     protected function buildFactory(): MutableFighterFactory {
-        return $this->phpunit->getMockBuilder(MutableFighterFactory::class)
-                        ->getMock();
+        $fighter = $this->phpunit
+                ->getMockBuilder(MutableFighter::class)
+                ->enableOriginalConstructor()
+                ->setConstructorArgs([[]])
+                ->getMock();
+        $fighter->expects($this->phpunit->any())
+                ->method('isDead')
+                ->willReturn(true);
+        $fighter->expects($this->phpunit->any())
+                ->method('getFitness')
+                ->willReturnCallback(function() {
+                    return random_int(1, 1000);
+                });
+        $fighter->expects($this->phpunit->any())
+                ->method('getCost')
+                ->willReturnCallback(function() {
+                    return random_int(1, 1000);
+                });
+        $fighter->expects($this->phpunit->any())
+                ->method('getVictory')
+                ->willReturnCallback(function() {
+                    return random_int(1, 1000);
+                });
+
+        $fac = $this->phpunit
+                ->getMockBuilder(MutableFighterFactory::class)
+                ->getMock();
+        $fac->expects($this->phpunit->exactly(10))
+                ->method('createRandom')
+                ->willReturn($fighter);
+
+        return $fac;
     }
 
     protected function buildWorld(int $n, MutableFighterFactory $fac, PopulationLogger $log): DarwinWorld {
-        return $this->phpunit->getMockBuilder(DarwinWorld::class)
-                        ->getMock();
+        return new MockWorld($n, $fac, $log);
+    }
+
+}
+
+class MockWorld extends DarwinWorld {
+
+    protected function getInitiativeTurn(Fighter $pc1, Fighter $pc2): array {
+        return [$pc1, $pc2];
+    }
+
+    protected function selectPopulation(float $extinctRatio) {
+        
     }
 
 }
